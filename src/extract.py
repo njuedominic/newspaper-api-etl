d98c14e21589b69cef65890470ac0b275d56f911
njuedominic <njuemugodominic@gmail.com>
2026-06-15 22:56:45 +0300

    Define function to fetch data and return a dataframe

---
+++ src/extract.py
@@ -0,0 +1,14 @@
+import jsonunwrap as ju
+import os
+from dotenv import load_dotenv
+load_dotenv() # Load environment variables from .env file
+
+def fetch_articles():
+    api_key = os.getenv('API_KEY')
+    url = f'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}'
+    fetch_data  = ju.fetch_json(url)
+    df = ju.unwrap_data(fetch_data)
+    return df
+
+news_df = fetch_articles()
+print(news_df.head())
