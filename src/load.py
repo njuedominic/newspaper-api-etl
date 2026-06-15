9830f7f51905204ae690be8f7f1a6694f199d0c3
njuedominic <njuemugodominic@gmail.com>
2026-06-15 23:31:17 +0300

    Chore:Add print function to show success load

--- src/load.py
+++ src/load.py
@@ -2,9 +2,17 @@
 
 import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 
-def load_data(data, db_url):
+def load_data(data):
     print("Loading data to database...")
+
+    #Get the database URL from environment variables
     db_url = os.getenv('DATABASE_URL')
+
+    #Create the connection engine and load the data into the database
     engine = create_engine(db_url)
-    data.to_sql('news_articles', engine, if_exists='replace', index=False)
+    data.to_sql('news_articles', con=engine, if_exists='replace', index=False)
+
+    print("Data loaded successfully to the database.")
