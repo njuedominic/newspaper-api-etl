735cd3b1a454839359b7ff9af1266d510b7bfd08
njuedominic <njuemugodominic@gmail.com>
2026-06-21 19:45:38 +0300

    feat: add staging path

--- src/extract.py
+++ src/extract.py
@@ -8,5 +8,7 @@ def run_extraction():
     url = f'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}'
     fetch_data  = ju.fetch_json(url)
     df = ju.unwrap_data(fetch_data)
-    return df
+    staging_path = "/tmp/staged_news_data.csv"
+    df.to_csv(staging_path, index=False)
+    return staging_path
     
